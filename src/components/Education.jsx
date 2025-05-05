f8f9e073c63881e052a8e750c106329c21a41f9d
sarvesh-shah <sarveshshah2000@gmail.com>
2025-05-05 03:18:13 -0400

    Alignments

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -10,18 +10,22 @@ export default function Education() {
         <div className="edu-card">
           <div className="edu-header">
             <FaUniversity className="edu-icon" />
+            <div>
             <h3>Pace University, New York, NY</h3>
+            <p className="sub-info">MS in Computer Science | GPA: 4.00 / 4.00 | May 2026</p>
+            </div>
           </div>
-          <p className="sub-info">MS in Computer Science | GPA: 4.00 / 4.00 | May 2026</p>
           <p>📘 OS, Java DS, AI, DBMS, Parallel Computing, Algorithms, Theory of Computation</p>
         </div>
 
         <div className="edu-card">
           <div className="edu-header">
             <FaGraduationCap className="edu-icon" />
-            <h3>Mumbai University, KJ Somaiya Institute</h3>
+            <div>
+              <h3>Mumbai University, KJ Somaiya Institute</h3>
+              <p className="sub-info">BTech in Electronics & Telecom | GPA: 8.2 / 10.0 | May 2022</p>
+            </div>
           </div>
-          <p className="sub-info">BTech in Electronics & Telecom | GPA: 8.2 / 10.0 | May 2022</p>
           <p>📘 Cloud Computing, NLP, Web Design, MIS, Image Processing</p>
         </div>
       </div>
